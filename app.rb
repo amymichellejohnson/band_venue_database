0463ace0d8d00fe633152b220fcfef8722455eee
amy.johnson.michelle@gmail.com <amy.johnson.michelle@gmail.com>
2015-01-30 14:12:58 -0800

    Add ability for user to add venues and bands and to list out venues with checkboxes to add venues the band has played

--- app.rb
+++ app.rb
@@ -2,3 +2,79 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  @bands= Band.all()
+  @venues = Venue.all()
+  erb(:index)
+end
+
+post("/bands") do
+  name = params.fetch("name")
+  Band.create({:name => name})
+  @bands=Band.all()
+  redirect("/")
+end
+
+get("/bands/:id") do
+  id = params.fetch("id")
+  @band = Band.find(id)
+  erb(:band_info)
+end
+
+delete("/bands/:id") do
+  id = params.fetch("id")
+  @band = Band.find(id)
+  @band.delete()
+  redirect("/")
+end
+
+patch("/bands/:id") do
+  id = params.fetch("id")
+  @band = Band.find(id)
+  name = params.fetch("name")
+  @band.update({:name =>name})
+  erb(:band_edit)
+end
+
+get("/bands/add_venues/:id") do
+  id = params.fetch("id")
+  @band = Band.find(id)
+  @venues= Venue.all()
+  erb(:band_add_venue)
+end
+
+patch("/bands/add_venues/:id") do
+  band_id = params.fetch("id")
+  @band = Band.find(band_id)
+  venue_ids = params.fetch("venue_ids")
+  @venues = Venue.all()
+  @band.update({:venue_ids => venue_ids})
+  erb(:band_add_venue)
+end
+
+get("/bands/edit/:id") do
+  id = params.fetch("id")
+  @band = Band.find(id)
+  erb(:band_edit)
+end
+
+# get("/bands/venues/delete/:id") do
+#   params.fetch('id')
+#
+#
+# end
+
+post("/venues") do
+  name = params.fetch("name")
+  Venue.create({:name => name})
+  @venues = Venue.all()
+  redirect("/")
+end
+
+get("/venues/delete/:id") do
+  id = params.fetch("id")
+  @venue = Venue.find(id)
+  @venue.delete()
+  redirect("/")
+end
--- lib/band.rb
+++ lib/band.rb
@@ -2,7 +2,7 @@ class Band <ActiveRecord::Base
   has_and_belongs_to_many(:venues)
   validates(:name, :presence => true)
   before_save(:capitalize)
-  default_scope(:order => 'bands.name() ASC')
+  # scope(:alphabetize, ->do where {order by Name ASC}) end)
 
 private
   define_method(:capitalize) do
